bf7b4e92b6068bf01f5718349262eb76734ff5a6
Katherine08 <dudarec08@gmail.com>
2016-11-09 22:50:06 +0200

    Add files via upload

---
+++ ideone_OOQYh5.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <cmath> 
+using namespace std; 
+
+int main() { 
+int k; 
+double t,x,eps,S; 
+cin>>x>>eps; 
+t=-x; 
+S=t; 
+printf("%15e,%20.4f\n", t, S);
+k=1; 
+while (fabs(t)>eps) 
+{ 
+t*=-pow(x,2)/(2*k*(2*k-1)); 
+S+=t; 
+printf("%15e%20.4f\n", t, S);
+k++;
+}
+cout<<"Summa="<<S;
+return 0; 
+}
