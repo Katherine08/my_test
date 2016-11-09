533648eeef48fa82d05a8ae0140754407ddf21b5
Bartlana <bartkovasvetlana@gmail.com>
2016-11-09 23:45:10 +0200

    Update ideone_OOQYh5.cpp

--- ideone_OOQYh5.cpp
+++ ideone_OOQYh5.cpp
@@ -3,20 +3,19 @@
 using namespace std; 
 
 int main() { 
-int k; 
-double t,x,eps,S; 
-cin>>x>>eps; 
-t=-x; 
-S=t; 
-printf("%15e,%20.4f\n", t, S);
-k=1; 
-while (fabs(t)>eps) 
-{ 
-t*=-pow(x,2)/(2*k*(2*k-1)); 
-S+=t; 
-printf("%15e%20.4f\n", t, S);
-k++;
-}
-cout<<"Summa="<<S;
-return 0; 
+  int k; 
+  double t,x,eps,S; 
+  cin>>x>>eps; 
+  t=-x; 
+  S=t; 
+  printf("%15e,%20.4f\n", t, S);
+  k=1; 
+  while (fabs(t)>eps) { 
+    t*=-pow(x,2)/(2*k*(2*k-1)); 
+    S+=t; 
+    printf("%15e%20.4f\n", t, S);
+    k++;
+  }
+  cout<<"Summa="<<S;
+  return 0; 
 }
